c4ff9943a19ff6c88b95ec1d34169ed68063ce76
Adi Kannatasik <75306794+reycvt@users.noreply.github.com>
2023-08-31 16:40:32 +0700

    Update install_nomad_grafana.sh

--- install_nomad_grafana.sh
+++ install_nomad_grafana.sh
@@ -4,16 +4,16 @@
 sudo apt update
 sudo apt install -y docker.io
 sudo usermod -aG docker ${USER}
-sudo usermod -aG docker ${USER}
 
 # Run Nomad container
-docker run -d --name=nomad \
-  --net=host \
-  --volume="$PWD:/nomad/config" \
-  hashicorp/nomad agent -config=/nomad/config/nomad.hcl
+# docker pull hashicorp/nomad
+# docker run -d --name=nomad \
+#   --net=host \
+#   --volume="$PWD:/nomad/config" \
+#   hashicorp/nomad agent -config=/nomad/config/nomad.hcl
 
-# Run Grafana container
-docker run -d --name=grafana \
-  --net=host \
-  -e "GF_INSTALL_PLUGINS=grafana-simple-json-datasource" \
-  grafana/grafana
+# # Run Grafana container
+# docker run -d --name=grafana \
+#   --net=host \
+#   -e "GF_INSTALL_PLUGINS=grafana-simple-json-datasource" \
+#   grafana/grafana
